d4971adcccaa9118820eb4a005b48e36c8c2f9ae
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-05-14 12:33:12 +0300

    Added player to game. Player can move forward.

--- js/game.js
+++ js/game.js
@@ -1,3 +1,38 @@
 'use strict';
 
-console.log('Let\'s playing');
+let gameScene = new Phaser.Scene('Game');
+
+gameScene.init = function() {
+  this.playerSpeed = 1;
+};
+
+gameScene.preload = function() {
+  this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png');
+  this.load.image('enemy', 'assets/enemy.png');
+  this.load.image('treasure', 'assets/treasure.png');
+};
+
+gameScene.create = function() {
+  let bg = this.add.sprite(0, 0, 'background');
+  bg.setOrigin(0, 0);
+  
+  this.player = this.add.sprite(40, this.sys.game.config.height/2, 'player');
+  
+  this.player.setScale(.4);
+};
+
+gameScene.update = function() {
+  if (this.input.activePointer.isDown) {
+    this.player.x += this.playerSpeed;
+  }
+};
+
+let config = {
+  type: Phaser.AUTO,
+  width: 640,
+  height: 360,
+  scene: gameScene
+};
+
+let game = new Phaser.Game(config);
