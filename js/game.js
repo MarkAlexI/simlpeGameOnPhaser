3dddd391e655c402d4590b529dfe0ef644c3609a
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-05-16 00:04:09 +0300

    Added enemies and game over.

--- js/game.js
+++ js/game.js
@@ -4,6 +4,8 @@ let gameScene = new Phaser.Scene('Game');
 
 gameScene.init = function() {
   this.playerSpeed = 1;
+  this.enemyMaxY = 280;
+  this.enemyMinY = 80;
 };
 
 gameScene.preload = function() {
@@ -22,12 +24,57 @@ gameScene.create = function() {
   
   this.treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height/2, 'treasure');
   this.treasure.setScale(.8);
+  
+  this.enemies = this.add.group({
+    key: 'enemy',
+    repeat: 5,
+    setXY: {
+      x: 110,
+      y: 100,
+      stepX: 80,
+      stepY: 20
+    }
+  });
+  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
+  
+  Phaser.Actions.Call(this.enemies.getChildren(), function(enemy) {
+    enemy.speed = Math.random() * 2 + 1;
+  }, this);
 };
 
 gameScene.update = function() {
+  if  (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.treasure.getBounds())) {
+    this.gameOver();
+  }
+  
   if (this.input.activePointer.isDown) {
     this.player.x += this.playerSpeed;
   }
+  
+  let enemies = this.enemies.getChildren();
+  let numEnemies = enemies.length;
+  for (let i = 0; i < numEnemies; i++) {
+    enemies[i].y += enemies[i].speed;
+    
+    if (enemies[i].y >= this.enemyMaxY && enemies[i].speed > 0) {
+      enemies[i].speed *= -1;
+    } else if (enemies[i].y <= this.enemyMinY && enemies[i].speed < 0) {
+      enemies[i].speed *= -1;
+    }
+    
+    if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemies[i].getBounds())) {
+      this.gameOver();
+      break;
+    }
+  }
+};
+
+gameScene.gameOver = function() {
+  this.cameras.main.shake(500);
+  
+  this.time.delayedCall(500, function() {
+    this.scene.restart();
+  }, [], this);
 };
 
 let config = {
